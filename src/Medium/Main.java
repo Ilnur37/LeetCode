2b27df875d8504150c10a3cd2e08be808a11c94d
Ilnur37 <dinmukhameto@mail.ru>
2022-08-18 15:25:54 +0300

    commit

---
+++ src/Medium/Main.java
@@ -0,0 +1,13 @@
+package Medium;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{-5,-3,-2,-1, 0, 8};
+        int[][] arr2 = new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        int[] arr3 = new int[]{2,7,11,15};
+        twoSum(arr3, 9);
+        sortedSquares(arr);
+        T54.spiralOrder(arr2);
+        T43.multiply("-34", "10");
+    }
+}
