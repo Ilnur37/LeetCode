eb3465ef5a9ea6d382d1776f31d07e4a33b076c2
Ilnur37 <dinmukhameto@mail.ru>
2022-08-19 12:51:44 +0300

    commit

---
+++ src/Medium/T103.java
@@ -0,0 +1,42 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class T103 {
+    static int deep = 0;
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> travers = new ArrayList<>();
+        deep = 0;
+        if (root == null) return travers;
+        List<Integer> row = new ArrayList<>();
+        row.add(root.val);
+        travers.add(row);
+        int lvl = 0;
+
+        checkChildren(root, ++lvl, travers);
+
+        return travers;
+    }
+
+    public static void checkChildren(TreeNode node, int lvl, List<List<Integer>> tr) {
+        if (lvl > deep && (node.left != null || node.right != null)) {
+            List<Integer> row = new ArrayList<>();
+            tr.add(row);
+            deep = lvl;
+        }
+        if (node.left != null) {
+            if (lvl % 2 != 0) tr.get(lvl).add(0, node.left.val);
+            else tr.get(lvl).add(node.left.val);
+            checkChildren(node.left, ++lvl, tr);
+            lvl--;
+
+        }
+        if (node.right != null) {
+            if (lvl % 2 != 0) tr.get(lvl).add(0, node.right.val);
+            else tr.get(lvl).add(node.right.val);
+            checkChildren(node.right, ++lvl, tr);
+            lvl--;
+        }
+    }
+}
