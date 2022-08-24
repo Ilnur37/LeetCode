be128b1a68da0d22e259978d86486d5a3ff7c2c1
Ilnur37 <dinmukhameto@mail.ru>
2022-08-24 20:20:34 +0300

    commit

--- src/Easy/Main.java
+++ src/Easy/Main.java
@@ -1,8 +1,17 @@
 package Easy;
 
+import Medium.ListNode;
+
 public class Main {
     public static void main(String[] args) {
         String a = "Let's take LeetCode contest";
-        T557.reverseWords(a);
+        //T557.reverseWords(a);
+        Easy.ListNode l1 = new Easy.ListNode(1);
+        Easy.ListNode l2 = new Easy.ListNode(2, l1);
+        Easy.ListNode l3 = new Easy.ListNode(3, l2);
+        Easy.ListNode l4 = new Easy.ListNode(2, l3);
+        Easy.ListNode l5 = new Easy.ListNode(1, l4);
+
+        System.out.println(T234.isPalindrome(l5));
     }
 }
---
+++ src/Medium/T994.java
@@ -0,0 +1,4 @@
+package Medium;
+
+public class T994 {
+}
